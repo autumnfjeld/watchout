de9df1dae03fa1e99ff033186e9e17b3d8987404
Hack Reactor Students <students@hackreactor.com>
2014-01-17 18:48:33 -0800

    enemy dots appear!

--- godots.js
+++ godots.js
@@ -1,25 +1,46 @@
 
 gameOptions = {
   height: 450,
-  width: 700,
+  width: 600,
   nEnemies: 30,
   padding: 20
 };
 
 
-var gameBoard = d3.select('body').append('svg')   // namespace:tag necesaary???
+var gameBoard = d3.select('body').append('svg:svg')
     .attr('width', gameOptions.width)
     .attr('height', gameOptions.height)
-    .style('background', 'blue');
+    .attr('backgroun-posistion', 50% 50%)
+    .style('background', '#1240AB');
 
-var data = "abcdefghijklmnopqrstuvwxyz".split("");
-//var data = "helloooo";
+  // returns array of objects with enemy properites
+ var createEnemies = function() {
+    return _.range(0, gameOptions.nEnemies).map(function(i) {
+      return {
+        id: i,
+        x: Math.random() * 100,
+        y: Math.random() * 100
+      };
+    });
+  };
 
-// note that this adds html attributes in the tag, rather than setting
-// css styling
-var text = gameBoard.selectAll("text").data(data)
-      .enter().append("text")
-      .attr("font-family", "sans-serif")
-      .attr("x", 30)
-      .attr("y", function(d,i){ return i*10;})
-      .text(function(d){ return d;});
+// this will linearly scale the input/domain to output/range
+// so we can work within the framework of 0 to 100 (see the Math.random line)
+var axes = {
+    x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
+    y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
+  };
+
+  var enemy_data = createEnemies();
+  console.log(enemy_data);
+
+ //circle.enemy = circle AND enemy
+    var enemies = gameBoard.selectAll('circle.enemy')
+        .data(enemy_data, function(d) {return d.id;});
+    enemies.enter().append('svg:circle')
+        .attr('class', 'enemy')
+        .attr('cx', function(enemy) {return axes.x(enemy.x);})
+        .attr('cy', function(enemy) {return axes.y(enemy.y);})
+        .attr('r', 20);
+    enemies.exit().remove();
+
